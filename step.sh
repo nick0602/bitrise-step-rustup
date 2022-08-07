4a673693f7a443dac1ac93a244f62a0e3384638a
Niccolò Forlini <nick@niccoloforlini.com>
2022-08-07 15:06:53 +0200

    refactor: update step.sh

--- step.sh
+++ step.sh
@@ -1,52 +1,46 @@
 #!/bin/bash
 set -e
 
-install_rustup_standard() {
-    curl https://sh.rustup.rs -sSf | sh -s -- -y
-}
+# Load input values that have been passed by Bitrise.
+source $BITRISE_STEP_SOURCE_DIR/inputs.sh
 
-install_rustup_nightly() {
-    install_rustup_standard
-    rustup toolchain install nightly --allow-downgrade --profile minimal --component clippy
-}
-
-set_default_rustup() {
-    if [ "$RUST_USE_RUSTUP_NIGHTLY" = true ]; then
-        rustup default nightly
-    else 
-        rustup default stable
-    fi
-}
+# Load required functions in scope to keep step.sh clean.
+source $BITRISE_STEP_SOURCE_DIR/helpers.sh
 
 # Silently source, in case the cache has already restored rustup and/or cargo folders.
-source "$HOME/.cargo/env" &> /dev/null || true
+source "$HOME/.cargo/env" &>/dev/null || true
 
-if ! command -v rustup &> /dev/null && [ "$RUST_USE_RUSTUP_NIGHTLY" = false ]; then
-    printf 'No Rust Toolchain found, installing latest stable...\n'
-    install_rustup_standard
-elif [ "$RUST_USE_RUSTUP_NIGHTLY" = true ]; then
-    printf 'Nightly Toolchain required, installing...\n'
-    install_rustup_nightly
-elif [ "$RUST_AUTO_UPDATE_TOOLCHAIN" = true ]; then
-    printf 'Toolchain auto-update enabled, running installation script again...\n'
-    install_rustup_standard
+# Keep track if it's first install to skip updates after the setup.
+IS_FIRST_INSTALL=false
+
+# Check if rustup is installed.
+if ! command -v rustup &>/dev/null; then
+    IS_FIRST_INSTALL=true
+
+    printf 'No Rust Toolchain found\n\n'
+    install_rustup
 fi
-
-# Calling set_default_rustup() to ensure the required version of rustc is used, as ~/.rustup/settings.toml might have been restored from cache.
-set_default_rustup
 
 # Export PATH via envman to make the toolchain available for the next steps.
 envman add --key "PATH" --value "${PATH}:$HOME/.cargo/bin"
 
-# Use envman to add versions as ENV
-envman run rustup -V | envman add --key CURRENT_RUSTUP_VERSION
-envman run rustc -V | envman add --key CURRENT_RUSTC_VERSION
-envman run cargo -V | envman add --key CURRENT_CARGO_VERSION
+# Calling set_default_rustup() to ensure the required version of rustc is used, as ~/.rustup/settings.toml might have been restored from cache.
+set_default_rustup
 
-# Show current versions
-printf "\n\nExported ENV vars:\n"
-envman run bash -c 'printf "CURRENT_RUSTUP_VERSION: $CURRENT_RUSTUP_VERSION"'
-envman run bash -c 'printf "CURRENT_RUSTC_VERSION: $CURRENT_RUSTC_VERSION"'
-envman run bash -c 'printf "CURRENT_CARGO_VERSION: $CURRENT_CARGO_VERSION"'
+# Update is not performed on first install as the download has just happened.
+if [ "$RUST_AUTO_UPDATE_TOOLCHAIN" = true ] && [ "$IS_FIRST_INSTALL" = false ]; then
+    update_rustup
+fi
+
+# Use envman to add versions as ENV.
+export_rust_envs
+
+# Append local cargo and rustup path to the local Bitrise cache if necessary.
+append_to_bitrise_cache
+
+# Show current versions.
+if [ "$RUST_SHOW_ENVS" = true ]; then
+    print_rust_envs
+fi
 
 exit $?
