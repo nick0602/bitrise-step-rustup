58cfe2a14202ede2e2f748f3509e5ec9ea8d87ca
Niccolò Forlini <nick@niccoloforlini.com>
2022-08-07 15:06:19 +0200

    refactor: handle inputs differently

---
+++ inputs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This file contains several variables used throughout the step, with the corresponding
+# input key value used during their definition.
+
+RUST_USE_RUSTUP_NIGHTLY=$use_rustup_nightly
+RUST_AUTO_UPDATE_TOOLCHAIN=$auto_update_toolchain
+RUST_CACHE_LEVEL=$cache_level
+RUST_SHOW_ENVS=$show_exported_envs
